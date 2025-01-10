2c7bd8283f41480363a68fe28a4b373a0288cdbc
mahmoudosman99 <devmahmoudosman@gmail.com>
2025-01-10 18:46:18 +0200

    make a connection checker file and test development

--- lib/core/helpers/internet_connection_checker.dart
+++ lib/core/helpers/internet_connection_checker.dart
@@ -1,29 +1,30 @@
-// import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import "package:flutter/material.dart";
 
-// Future<void> checkInternetConnectivity() async {
-//   var connectivityResult = await Connectivity().checkConnectivity();
-//   switch (connectivityResult) {
-//     case ConnectivityResult.mobile:
-//       break;
-//     default:
-//   }
-//   if (connectivityResult == ConnectivityResult.mobile) {
-//     // I am connected to a mobile network.
-//     print('Connected to mobile data');
-//   } else if (connectivityResult == ConnectivityResult.wifi) {
-//     // I am connected to a wifi network.
-//     print('Connected to Wi-Fi');
-//   } else if (connectivityResult == ConnectivityResult.ethernet) {
-//     // I am connected to a ethernet network.
-//     print('Connected to ethernet');
-//   } else if (connectivityResult == ConnectivityResult.bluetooth) {
-//     // I am connected to a bluetooth network.
-//     print('Connected to bluetooth');
-//   } else if (connectivityResult == ConnectivityResult.other) {
-//     // I am connected to a network which is not mobile, wifi or ethernet.
-//     print('Connected to other');
-//   } else if (connectivityResult == ConnectivityResult.none) {
-//     // I am not connected to any network.
-//     print('No internet connection');
-//   }
-// }
+Future<void> checkInternetConnectivity() async {
+  var connectivityResult = await Connectivity().checkConnectivity();
+  switch (connectivityResult) {
+    case ConnectivityResult.mobile:
+      break;
+    default:
+  }
+  if (ConnectivityResult.values.contains(ConnectivityResult.mobile)) {
+    // I am connected to a mobile network.
+    debugPrint('Connected to mobile data');
+  } else if (ConnectivityResult.values.contains(ConnectivityResult.wifi)) {
+    // I am connected to a wifi network.
+    debugPrint('Connected to Wi-Fi');
+  } else if (connectivityResult == ConnectivityResult.ethernet) {
+    // I am connected to a ethernet network.
+    debugPrint('Connected to ethernet');
+  } else if (connectivityResult == ConnectivityResult.bluetooth) {
+    // I am connected to a bluetooth network.
+    debugPrint('Connected to bluetooth');
+  } else if (connectivityResult == ConnectivityResult.other) {
+    // I am connected to a network which is not mobile, wifi or ethernet.
+    debugPrint('Connected to other');
+  } else if (connectivityResult == ConnectivityResult.none) {
+    // I am not connected to any network.
+    debugPrint('No internet connection');
+  }
+}
